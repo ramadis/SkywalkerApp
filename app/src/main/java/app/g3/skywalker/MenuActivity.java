9dba14d1874e00f0efd9d25c732626b327907db2
Rama Olivera Fedi <rramiro.o@hotmail.com>
2017-06-20 18:37:02 -0300

    Add link to map

--- app/src/main/java/app/g3/skywalker/MenuActivity.java
+++ app/src/main/java/app/g3/skywalker/MenuActivity.java
@@ -1,5 +1,6 @@
 package app.g3.skywalker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -83,6 +84,9 @@ public class MenuActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+            Intent myIntent = new Intent(MenuActivity.this, MapDealActivity.class);
+            //myIntent.putExtra("key", value); //Optional parameters
+            MenuActivity.this.startActivity(myIntent);
 
         } else if (id == R.id.nav_slideshow) {
 
